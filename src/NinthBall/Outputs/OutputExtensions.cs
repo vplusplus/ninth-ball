eac5788d95557ab55065fdfabbb94f9e7531d5c3
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-04 17:09:15 -0500

    Html output - Fine tuning

--- src/NinthBall/Outputs/OutputExtensions.cs
+++ src/NinthBall/Outputs/OutputExtensions.cs
@@ -21,5 +21,19 @@ namespace NinthBall
 
             return 0 == count ? 0.0 : Math.Pow(compoundReturn, 1.0 / count) - 1;
         }
+
+        public static double SumX(this ReadOnlyMemory<SimYear> byYear, Func<SimYear, double> fxValueSelector)
+        {
+            ArgumentNullException.ThrowIfNull(fxValueSelector);
+
+            double sumAmount = 0.0;
+
+            for (int i = 0; i < byYear.Length; i++)
+            {
+                sumAmount += fxValueSelector(byYear.Span[i]);
+            }
+
+            return sumAmount;
+        }
     }
 }
