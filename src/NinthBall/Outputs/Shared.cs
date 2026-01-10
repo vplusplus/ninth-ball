358f5922a3d8702eac9ff06525f0a46dad4edc27
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-09 23:43:03 -0500

    WIP - Output generalization

--- src/NinthBall/Outputs/Shared.cs
+++ src/NinthBall/Outputs/Shared.cs
@@ -6,6 +6,7 @@ using static NinthBall.Outputs.WidthHint;
 using static NinthBall.Outputs.AlignHint;
 using static NinthBall.Outputs.ColorHint;
 
+
 namespace NinthBall.Outputs
 {
     internal enum FormatHint { F0, C0, C1, C2, P0, P1, P2 }
@@ -23,26 +24,48 @@ namespace NinthBall.Outputs
         SS, Ann, Incomes,
         XPreTax, XPostTax, XCash, XTotal,
         Dec, DecValue, DecPreTax, DecPreTaxAlloc, DecPostTax, DecPostTaxAlloc, DecCash, DecTotal, DecApprox,
-        LikeYear, ROIStocks, ROIBonds, ROICash,
+        LikeYear, ROI, ROIStocks, ROIBonds, ROICash,
     }
 
     internal static class ColumnDefinitions
     {
-        delegate ColorHint ColorSelector(SimIteration iteration, SimYear year);
+        public static readonly IReadOnlyList<CID> DefaultColumns =
+        [
+            CID.Year, CID.Age,
+            CID.JanPreTax, CID.JanPostTax, CID.JanValue,
+            CID.DecValue,
+            CID.LikeYear, CID.ROI, CID.ROIStocks, CID.ROIBonds
+        ];
 
-        static readonly IReadOnlyDictionary<CID, Func<SimYear, double>> Values = new Dictionary<CID, Func<SimYear, double>>()
+        internal static WidthHint GetWidthHint(this CID cid) => Widths.TryGetValue(cid, out var widthHint) ? widthHint : WDefault;
+        internal static AlignHint GetAlignmentHint(this CID cid) => Alignments.TryGetValue(cid, out var alignHint) ? alignHint : AlignHint.Right;
+        internal static FormatHint GetFormatHint(this CID cid) => Formats.TryGetValue(cid, out var hint) ? hint : FormatHint.C0;
+        internal static string GetColName(this CID cid) => ColumnNames.TryGetValue(cid, out var colName) && null != colName ? colName : cid.ToString();
+        internal static string GetColTitle(this CID cid) => ColumnTitles.TryGetValue(cid, out var colTitle) && null != colTitle ? colTitle : string.Empty;
+        internal static double? GetCellValue(this SimYear simYear, CID cid) => FxValues.TryGetValue(cid, out var fxValue) && null != fxValue ? fxValue(simYear) : null;
+        internal static ColorHint GetCellColorHint(this SimYear y, CID cid, SimIteration iter) => FxColors.TryGetValue(cid, out var fxColor) && null != fxColor ? fxColor(iter, y) : ColorHint.None;
+
+
+
+
+        delegate ColorHint ColorSelector(SimIteration iteration, SimYear year);
+        delegate double ValueSelector(SimYear simYear);
+        delegate double SumValueSelector(SimIteration simIeration);
+
+
+        static readonly IReadOnlyDictionary<CID, ValueSelector> FxValues = new Dictionary<CID, ValueSelector>()
         {
             [CID.Year] = y => y.Year + 1,
             [CID.Age] = y => y.Age,
 
             [CID.Jan] = y => y.Jan.Total(),
-            [CID.JanValue] = y => y.Jan.ApproxValue(),
+            [CID.JanValue] = y => y.Jan.ApproxValue,
             [CID.JanPreTax] = y => y.Jan.PreTax.Amount,
             [CID.JanPostTax] = y => y.Jan.PostTax.Amount,
             [CID.JanCash] = y => y.Jan.Cash.Amount,
 
             [CID.Dec] = y => y.Dec.Total(),
-            [CID.DecValue] = y => y.Dec.ApproxValue(),
+            [CID.DecValue] = y => y.Dec.ApproxValue,
             [CID.DecPreTax] = y => y.Dec.PreTax.Amount,
             [CID.DecPostTax] = y => y.Dec.PostTax.Amount,
             [CID.DecCash] = y => y.Dec.Cash.Amount,
@@ -57,18 +80,19 @@ namespace NinthBall.Outputs
             [CID.ROIStocks] = y => y.ROI.StocksROI,
             [CID.ROIBonds] = y => y.ROI.BondsROI,
             [CID.ROICash] = y => y.ROI.CashROI,
+            [CID.ROI] = y => y.EffectiveROI,
 
         }.AsReadOnly();
 
-        static readonly IReadOnlyDictionary<CID, Func<SimIteration, double>> Aggregates = new Dictionary<CID, Func<SimIteration, double>>()
+        static readonly IReadOnlyDictionary<CID, SumValueSelector> FxAggregates = new Dictionary<CID, SumValueSelector>()
         {
-            [CID.Year] = c => c.Max(y => y.Year + 1),
-            [CID.Age] = c => c.Max(y => y.Age),
+            [CID.Year]      = iter => iter.Max(y => y.Year + 1),
+            [CID.Age]       = iter => iter.Max(y => y.Age),
 
-            [CID.Jan] = c => c.Sum(y => y.Jan.Total()),
-            [CID.JanValue] = c => c.Sum(y => y.Jan.ApproxValue()),
-            [CID.Dec] = c => c.Sum(y => y.Dec.Total()),
-            [CID.DecValue] = c => c.Sum(y => y.Dec.ApproxValue()),
+            [CID.Jan]       = iter => iter.Sum(y => y.Jan.Total()),
+            [CID.JanValue]  = iter => iter.Sum(y => y.Jan.ApproxValue),
+            [CID.Dec]       = iter => iter.Sum(y => y.Dec.Total()),
+            [CID.DecValue]  = iter => iter.Sum(y => y.Dec.ApproxValue),
 
         }.AsReadOnly();
 
@@ -86,6 +110,7 @@ namespace NinthBall.Outputs
             [CID.ROIStocks] = P1,
             [CID.ROIBonds]  = P1,
             [CID.ROICash]   = P1,
+            [CID.ROI]       = P1,
 
         }.AsReadOnly();
 
@@ -103,83 +128,94 @@ namespace NinthBall.Outputs
             [CID.Age] = WSmall,
 
             [CID.LikeYear] = WSmall,
+            [CID.ROI] = WSmall,
             [CID.ROIStocks] = WSmall,
             [CID.ROIBonds] = WSmall,
-            [CID.DecCash] = WSmall,
+            [CID.ROICash] = WSmall,
+
+        }.AsReadOnly();
+
+        static readonly IReadOnlyDictionary<CID, ColorSelector> FxColors = new Dictionary<CID, ColorSelector>()
+        {
+            [CID.LikeYear]  = (it, y) => ROIRedGreyGreen(y),
+            [CID.ROI]       = (it, y) => ROIRedGreyGreen(y),
+            [CID.ROIStocks] = (it, y) => ROIRedGreyGreen(y.ROI.StocksROI),
+            [CID.ROIBonds]  = (it, y) => ROIRedGreyGreen(y.ROI.BondsROI),
+            [CID.ROICash]   = (it, y) => ROIRedGreyGreen(y.ROI.CashROI),
+
+        }.AsReadOnly();
+
+        static readonly IReadOnlyDictionary<CID, string> ColumnNames = new Dictionary<CID, string>()
+        {
+            [CID.Year] = "Yr",
+            
+            [CID.JanValue]    = "~Value",
+            [CID.DecValue]    = "~Value",
+
+            [CID.JanPreTax]   = "Jan 401K",
+            [CID.JanPostTax]  = "Jan Inv",
+            [CID.DecPreTax]   = "Dec 401K",
+            [CID.DecPostTax]  = "Dec Inv",
+
+            [CID.LikeYear]    = "Like",
+            [CID.ROI]         = "ROI",
+            [CID.ROIStocks]   = "Stocks",
+            [CID.ROIBonds]    = "Bonds",
+
+        }.AsReadOnly();
+
+        static readonly IReadOnlyDictionary<CID, string> ColumnTitles = new Dictionary<CID, string>()
+        {
+            [CID.ROI] = "Effective ROI (StocAlloc x StockROI + BondAlloc x BondROI)",
+            [CID.JanValue] = "Approx value (401K x 75% + Inv x 85%)",
+            [CID.DecValue] = "Approx value (401K x 75% + Inv x 85%)"
 
         }.AsReadOnly();
 
 
+        static ColorHint ROIRedGreyGreen(SimYear simYear) => ROIRedGreyGreen(simYear.EffectiveROI);
+        static ColorHint ROIRedGreyGreen(double pctValue) => pctValue >= -0.04 && pctValue <= +0.04 ? ColorHint.Muted : pctValue <= 0 ? ColorHint.Danger : ColorHint.Success;
 
-        static double ApproxValue(this Assets assets) => (assets.PreTax.Amount * 0.75) + (assets.PostTax.Amount * 0.85) + assets.Cash.Amount;
 
-        static double Max(this SimIteration iteration, Func<SimYear, double> fxValueSelector)
+        extension (Assets assets)
         {
-            var span = iteration.ByYear.Span;
-            double value = 0.0;
-            for (int i = 0; i < iteration.ByYear.Length; i++) value = Math.Max(value, fxValueSelector(span[i]));
-            return value;
+            private double ApproxValue => (assets.PreTax.Amount * 0.75) + (assets.PostTax.Amount * 0.85) + assets.Cash.Amount;
         }
 
-        static double Sum(this SimIteration iteration, Func<SimYear, double> fxValueSelector)
+        extension (Asset asset)
         {
-            var span = iteration.ByYear.Span;
-            double sumAmount = 0.0;
-            for (int i = 0; i < iteration.ByYear.Length; i++) sumAmount += fxValueSelector(span[i]);
-            return sumAmount;
+            private double StockAlloc => asset.Allocation;
+            private double BondAlloc => 1.0 - asset.Allocation;
         }
 
-
-        internal static WidthHint GetWidthHint(this CID cid)
+        extension (SimYear simYear)
         {
-            return Widths.TryGetValue(cid, out var widthHint) ? widthHint : WDefault;
+            private double EffectiveROI =>
+                (0.0
+                    + simYear.ROI.StocksROI * simYear.Jan.PreTax.StockAlloc
+                    + simYear.ROI.BondsROI * simYear.Jan.PreTax.BondAlloc
+                    + simYear.ROI.StocksROI * simYear.Jan.PostTax.StockAlloc
+                    + simYear.ROI.BondsROI * simYear.Jan.PostTax.BondAlloc
+                ) / 2.0;
+        }        
+
+        extension (SimIteration iteration)
+        {
+            private double Max(Func<SimYear, double> fxValueSelector)
+            {
+                var span = iteration.ByYear.Span;
+                double value = 0.0;
+                for (int i = 0; i < iteration.ByYear.Length; i++) value = Math.Max(value, fxValueSelector(span[i]));
+                return value;
+            }
+
+            private double Sum(Func<SimYear, double> fxValueSelector)
+            {
+                var span = iteration.ByYear.Span;
+                double sumAmount = 0.0;
+                for (int i = 0; i < iteration.ByYear.Length; i++) sumAmount += fxValueSelector(span[i]);
+                return sumAmount;
+            }
         }
-
-        internal static AlignHint GetAlignmentHint(this CID cid)
-        {
-            return Alignments.TryGetValue(cid, out var alignHint) ? alignHint : AlignHint.Right;
-        }
-
-        internal static FormatHint GetFormatHint(this CID cid)
-        {
-            return Formats.TryGetValue(cid, out var hint) ? hint : FormatHint.C0;
-        }
-
-        internal static string GetColName(this CID cid)
-        {
-            return cid.ToString();
-        }
-
-        internal static string GetColTitle(this CID cid)
-        {
-            return cid.ToString();
-        }
-
-        internal static double? GetCellValue(this SimYear simYear, CID cid)
-        {
-            var isDefined = Values.TryGetValue(cid, out var fxValue);
-            return isDefined && null != fxValue ? fxValue(simYear) : null;
-        }
-
-        internal static ColorHint GetCellColorHint(this SimYear y, CID cid, SimIteration iter)
-        {
-            return ColorHint.None;
-        }
-
-
-
-        public static IReadOnlyList<CID> DefaultColumns { get; } = new CID[]
-        {
-            CID.Year, CID.Age,
-            CID.JanPreTax, CID.JanPostTax, CID.JanValue,
-            CID.DecValue,
-            CID.LikeYear, CID.ROIStocks, CID.ROIBonds, CID.ROICash,
-        };
-
-
     }
 }
-
-
-
-
