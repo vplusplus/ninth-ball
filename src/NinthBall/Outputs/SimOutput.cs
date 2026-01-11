27e431b022e9f6a7e40ee6c614937a8a4e80f0ef
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-11 17:26:22 -0500

    pcttl label.

--- src/NinthBall/Outputs/SimOutput.cs
+++ src/NinthBall/Outputs/SimOutput.cs
@@ -4,7 +4,15 @@ namespace NinthBall.Outputs
     public readonly record struct Percentile(double Pctl, string FriendlyName)
     {
         public const double Target = 0.20;
-        public string PctlName => $"{Pctl * 100:0}th";
+        public string PctlName
+        {
+            get
+            {
+                int val = (int)Math.Round(Pctl * 100);
+                string suffix = val % 100 is >= 11 and <= 13 ? "th" : (val % 10) switch { 1 => "st", 2 => "nd", 3 => "rd", _ => "th" };
+                return $"{val}{suffix}";
+            }
+        }
     }
 
     public readonly record struct SimOutput
