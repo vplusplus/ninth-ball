d358be5c46a2e5bae3d06e4c57b6ec459043580a
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-19 15:25:38 -0500

    WIP - Safety check-in. No value.

--- src/UnitTests/DeleteMe.cs
+++ src/UnitTests/DeleteMe.cs
@@ -1,9 +1,26 @@
 ﻿
-using NinthBall.Core;
 
 namespace UnitTests
 {
-   
+    public readonly record struct Taxes(Taxes.Inc GrossIncome, Taxes.TD Federal, Taxes.TD State)
+    {
+        public readonly record struct Inc(double OrdInc, double INT, double DIV, double LTCG)
+        {
+            public readonly double Total => OrdInc + INT + DIV + LTCG;
+        }
+
+        public readonly record struct TR(double OrdInc, double LTCG);
+
+        public readonly record struct TD(double Deduction, double Taxable, TR MarginalRate, double Tax)
+        {
+            public readonly double EffectiveRate => Taxable < 0.01 ? 0.0 : Tax / Taxable;
+        }
+
+        public readonly double Total => Federal.Tax + State.Tax;
+
+        public readonly double EffectiveRate => GrossIncome.Total < 0.01 ? 0.0 : Total / GrossIncome.Total;
+    }
+
 
 }
 
