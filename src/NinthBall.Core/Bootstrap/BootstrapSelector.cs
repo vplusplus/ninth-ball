765f644325ed272f651b1b30caa359369cd3631a
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-24 12:51:56 -0500

    Cosmetic

--- src/NinthBall.Core/Bootstrap/BootstrapSelector.cs
+++ src/NinthBall.Core/Bootstrap/BootstrapSelector.cs
@@ -3,18 +3,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace NinthBall.Core
 {
-    internal sealed class BootstrapSelector(IServiceProvider AvailableServices, Growth GrowthObjective)
+    internal sealed class BootstrapSelector(IServiceProvider AvailableServices, Growth GrowthOptions)
     {
         public IBootstrapper GetSelectedBootstrapper()
         {
-            return GrowthObjective.Bootstrapper switch
+            return GrowthOptions.Bootstrapper switch
             {
                 BootstrapKind.Flat          => AvailableServices.GetRequiredService<FlatBootstrapper>(),
                 BootstrapKind.Sequential    => AvailableServices.GetRequiredService<SequentialBootstrapper>(),
                 BootstrapKind.MovingBlock   => AvailableServices.GetRequiredService<MovingBlockBootstrapper>(),
                 BootstrapKind.Parametric    => AvailableServices.GetRequiredService<ParametricBootstrapper>(),
 
-                _ => throw new NotSupportedException($"Unknown bootstrapper kind: {GrowthObjective.Bootstrapper}"),
+                _ => throw new NotSupportedException($"Unknown bootstrapper kind: {GrowthOptions.Bootstrapper}"),
             };
         }
     }
