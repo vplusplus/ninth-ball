c740608abb025b3699de52bfd6c324060a5bbbf1
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-26 18:17:49 -0500

    Cosmetic.

--- src/NinthBall.Core/SimObjectivesAndStrategies/ZeroObjectives.cs
+++ src/NinthBall.Core/SimObjectivesAndStrategies/ZeroObjectives.cs
@@ -8,11 +8,17 @@ namespace NinthBall.Core
     // For what‑if runs, use a NoOp objective to explicitly mute any objective you want to ignore.
     abstract class NoOpObjective : ISimObjective, ISimStrategy
     {
+        // Push to end.
         int ISimObjective.Order => int.MaxValue;
-        ISimStrategy ISimObjective.CreateStrategy(int _) => this;
-        void ISimStrategy.Apply(ISimState _) { }
-        public override string ToString() => Humanize(this.GetType().Name);
 
+        // I am the objective and I am the strategy.
+        ISimStrategy ISimObjective.CreateStrategy(int _) => this;
+
+        // Do nothing.
+        void ISimStrategy.Apply(ISimState _) { }
+
+        public override string ToString() => Humanize(this.GetType().Name);
+        
         static string Humanize(string input) => RxCaps.Replace(input.Replace("Objective", string.Empty), " $1");
 
         static readonly Regex RxCaps= new("(?<!^)([A-Z])", RegexOptions.Compiled);
