0cfc242d33245236bb090beb71939adb2ece642c
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-01-30 22:07:12 -0500

    Cosmetic

--- src/NinthBall.Reports/Utils/Print.cs
+++ src/NinthBall.Reports/Utils/Print.cs
@@ -5,6 +5,7 @@ namespace NinthBall.Reports
     static class Print
     {
         static readonly string DASHES = new('-', 90);
+
         public static void See(string substance, string fileName)
         {
             Console.WriteLine($" {substance,-20} | See {fileName}");
