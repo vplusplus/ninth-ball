094be91bd923dfa5f8bc0552a0785681105b33e0
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-02-13 21:20:57 -0500

    WIP: Fluent API for what-if overrides

--- src/UnitTests/MultipleSimulations.cs
+++ src/UnitTests/MultipleSimulations.cs
@@ -13,22 +13,6 @@ namespace UnitTests
     public class MultipleSimulations
     {
         [TestMethod]
-        public void MemberPathTests()
-        {
-            string[] paths = 
-            [
-                ConfigPath.GetPropertyPath<SimParams, double>(x => x.NoOfYears),
-                ConfigPath.GetPropertyPath<SimParams, double>(x => x.StartAge),
-                ConfigPath.GetPropertyPath<Initial,   double>(x => x.PreTax.Allocation),
-                ConfigPath.GetPropertyPath<Initial,   double>(x => x.PreTax.Amount),
-                ConfigPath.GetPropertyPath<Initial,   double>(x => x.PostTax.Allocation),
-                ConfigPath.GetPropertyPath<Initial,   double>(x => x.PostTax.Amount),
-            ];
-
-            foreach (var item in paths) Console.WriteLine(item);
-        }
-
-        [TestMethod]
         public async Task RunTestSimulations()
         {
             // Target objectives
@@ -45,10 +29,15 @@ namespace UnitTests
             foreach (var objective in growthObjectives)
             {
                 // Prepare overrides
-                var overrides = new InputOverrides()
-                    //.InitialPreTaxAmount(2000000, baseConfig)
-                    //.InitialPostTaxAmount(2000000, baseConfig)
-                    .WithObjective(objective, baseConfig);
+                InputOverrides overrides = new InputOverrides()
+                    .For<Initial>()
+                        .With(x => x.PreTax.Amount, 1000000)
+                        .With(x => x.PostTax.Amount, 1000000)
+                    .For<SimParams>()
+                        // .With(x => x.Iterations, 100)
+                        // .With(x => x.NoOfYears, 30)
+                        .Append(x => x.Objectives, objective, baseConfig)
+                    ;
 
                 // We create and destroy DI container for each simulation session.
                 var simSessionBuilder = Host.CreateEmptyApplicationBuilder(settings: new());
@@ -85,87 +74,3 @@ namespace UnitTests
     }
 }
 
-/*
-         [TestMethod]
-        public async Task RunTestSimulations()
-        {
-            var now = DateTime.Now;
-            var baseDir = @$"D:/Junk/SimReports-{now:MMdd-HHmmss}/";
-
-            if (!Directory.Exists(baseDir)) Directory.CreateDirectory(baseDir);
-
-            string[] growthObjectives = ["FlatGrowth", "HistoricalGrowth", "RandomHistoricalGrowth", "ExpectedGrowth", "ConservativeGrowth", "HighRiskGrowth"];
-
-            var overrides = new InputOverrides();
-
-
-            var cfg = new ConfigurationBuilder()
-                .AddSimulationDefaults()
-                .AddReportDefaults()
-                .AddYamlResources(typeof(MultipleSimulations).Assembly, ".TestInputs.")
-                // HERE I would like to read an existing config which as an array of string and append another item
-                // .AddOneStringToExistingStringArray("My:Section:Property", "Test")
-                .AddInMemoryCollection(overrides!)
-                .Build();
-
-            foreach(var growthObjective in growthObjectives)
-            {
-                var htmlFileName = Path.Combine(baseDir, $"{growthObjective}.html");
-
-                var oldParams = cfg.GetSection("SimParams").Get<SimParams>();
-                string[] newObjectives = [.. oldParams!.Objectives, growthObjective];
-                var newParams = oldParams! with
-                {
-                    Objectives = newObjectives
-                };
-
-                var oldOutput = cfg.GetSection("Outputs").Get<OutputOptions>();
-                var newOutput = oldOutput! with
-                {
-                    Html = oldOutput.Html with
-                    {
-                        File = htmlFileName
-                    }
-                };
-
-                await RunSimulation(newParams, newOutput);
-            }
-        }
-
-        private static async Task RunSimulation(params object[] overrides)
-        {
-            // We create and destroy DI container for each simulation session.
-            var simSessionBuilder = Host.CreateEmptyApplicationBuilder(settings: new());
-
-            // Apply default choices and user overrides.
-            simSessionBuilder.Configuration
-                .AddSimulationDefaults()
-                .AddReportDefaults()
-                .AddYamlResources(typeof(MultipleSimulations).Assembly, ".TestInputs.")
-                ;
-
-            // Assemble simulation and reporting components.
-            simSessionBuilder.Services
-                .AddSimulationComponents()
-                .AddReportComponents()
-                ;
-
-            // Apply overrides
-            // foreach (var obj in overrides) if (null != obj) simSessionBuilder.Services.AddSingleton(obj.GetType(), obj);
-
-            // Run simulation, export reports
-            using (var simSession = simSessionBuilder.Build())
-            {
-                // Resolve required services
-                ISimulation simulation = simSession.Services.GetRequiredService<ISimulation>();
-                ISimulationReports simReports = simSession.Services.GetRequiredService<ISimulationReports>();
-
-                var simResults = simulation.Run();
-                await simReports.GenerateAsync(simResults);
-
-                var sr = simResults.SurvivalRate;
-                var txtSurvivalRate = sr > 0.99 ? $"{sr:P1}" : $"{sr:P0}";
-                Console.WriteLine($" Survival rate: {txtSurvivalRate}");
-            }
-        }
-*/
--- src/UnitTests/WhatIf/InputOverrides.cs
+++ src/UnitTests/WhatIf/InputOverrides.cs
@@ -1,15 +1,42 @@
 ﻿using Microsoft.Extensions.Configuration;
-using NinthBall.Core;
-using System.Globalization;
 using System.Linq.Expressions;
 
 namespace UnitTests.WhatIf
 {
     public sealed class InputOverrides : Dictionary<string, string?> { }
 
-    public static class ConfigPath 
+    public static class Overridable
     {
-        public static string GetPropertyPath<T, TProperty>(Expression<Func<T, TProperty>> expression)
+        public static Override<T> For<T>(this InputOverrides overrides) => new(overrides);
+    }
+
+    public readonly record struct Override<T>(InputOverrides Overrides)
+    {
+        // Set single value
+        public Override<T> With<TValue>(Expression<Func<T, TValue>> expression, TValue value)
+        {
+            var propertyPath = GetPropertyPath(expression);
+            Overrides[propertyPath] = value?.ToString();
+            return this;
+        }
+
+        public Override<T> Append<TValue>(Expression<Func<T, TValue>> expression, string value, IConfiguration baseConfig)
+        {
+            // We leverage the fact that all IConfiguration primitives are string(s)
+            // Append() expects an array style property
+
+            var propertyPathPrefix = GetPropertyPath(expression);
+            var oneOrMoreValues = GetArrayValues(baseConfig, propertyPathPrefix);
+            oneOrMoreValues.Add(value);
+            SetArrayValues(Overrides, propertyPathPrefix, oneOrMoreValues);
+            
+            return this;
+        }
+
+        // Lead to next 
+        public Override<TNext> For<TNext>() => new (Overrides);
+
+        private static string GetPropertyPath<TProperty>(Expression<Func<T, TProperty>> expression)
         {
             var members = new Stack<string>();
 
@@ -26,48 +53,8 @@ namespace UnitTests.WhatIf
             var tail = members.Count == 0 ? string.Empty : $":{string.Join(":", members)}";
             return $"{typeof(T).Name}{tail}";
         }
-    }
 
-    public static class InputOverrideExtensions
-    {
-
-        public static InputOverrides InitialPreTaxAmount(this InputOverrides overrides, double value, IConfiguration baseConfig)
-        {
-            overrides[ConfigPath.GetPropertyPath<Initial, double>(x => x.PreTax.Amount)] = value.ToString(CultureInfo.InvariantCulture);
-            return overrides;
-        }
-
-        public static InputOverrides InitialPreTaxAllocation(this InputOverrides overrides, double value, IConfiguration baseConfig)
-        {
-            overrides[ConfigPath.GetPropertyPath<Initial, double>(x => x.PreTax.Allocation)] = value.ToString(CultureInfo.InvariantCulture);
-            return overrides;
-        }
-
-        public static InputOverrides InitialPostTaxAmount(this InputOverrides overrides, double value, IConfiguration baseConfig)
-        {
-            overrides[ConfigPath.GetPropertyPath<Initial, double>(x => x.PostTax.Amount)] = value.ToString(CultureInfo.InvariantCulture);
-            return overrides;
-        }
-
-        public static InputOverrides InitialPostTaxAllocation(this InputOverrides overrides, double value, IConfiguration baseConfig)
-        {
-            overrides[ConfigPath.GetPropertyPath<Initial, double>(x => x.PostTax.Allocation)] = value.ToString(CultureInfo.InvariantCulture);
-            return overrides;
-        }
-
-        public static InputOverrides WithObjective(this InputOverrides overrides, string value, IConfiguration baseConfig)
-        {
-            var pathPrefix = ConfigPath.GetPropertyPath<SimParams, IReadOnlyList<string>>(x => x.Objectives);
-
-            var objectives = baseConfig.GetArrayValues(pathPrefix);
-            objectives.Add(value);
-            overrides.SetArrayValues(pathPrefix, objectives);
-            
-            return overrides;
-        }
-
-
-        private static List<string> GetArrayValues(this IConfiguration baseConfig, string pathPrefix)
+        private static List<string> GetArrayValues(IConfiguration baseConfig, string pathPrefix)
         {
             return baseConfig
                 .GetSection(pathPrefix)
@@ -77,14 +64,15 @@ namespace UnitTests.WhatIf
                 .ToList()!;
         }
 
-        private static void SetArrayValues(this InputOverrides overrides, string pathPrefix, IReadOnlyList<string> values)
+        private static void SetArrayValues(InputOverrides overrides, string pathPrefix, IReadOnlyList<string> values)
         {
-            for(int i=0; i<values.Count; i++)
+            for (int i = 0; i < values.Count; i++)
             {
                 overrides[$"{pathPrefix}:{i}"] = values[i];
             }
         }
 
-    }
+        public static implicit operator InputOverrides(Override<T> context) => context.Overrides;
 
+    }
 }
