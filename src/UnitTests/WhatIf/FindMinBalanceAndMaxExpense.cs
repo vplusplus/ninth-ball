6324dab2ecca243e08a4de0bb8c4dfba1ff7548c
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-02-28 15:46:44 -0500

    Optmized what-if report

--- src/UnitTests/WhatIf/FindMinBalanceAndMaxExpense.cs
+++ src/UnitTests/WhatIf/FindMinBalanceAndMaxExpense.cs
@@ -2,9 +2,12 @@ using Microsoft.Extensions.Configuration;
 using NinthBall.Core;
 using NinthBall.Reports.PrettyPrint;
 using System.Data;
+using System.Diagnostics;
 
 namespace UnitTests.WhatIf
 {
+    using MultiSimResult = (double InitBal, double FYExp, double SurvivalRate, double RBal5th, double RBal10th, double RBal20th);
+
     public partial class MultipleSimulations
     {
         [TestMethod]
@@ -13,72 +16,95 @@ namespace UnitTests.WhatIf
             const string ReportFileName = "MinBalanceAndMaxExpense.md";
 
             // Given initial balance, find first year expense
-            const double InitBalance    = 3_500_000;
             const double MinExpense     =   120_000;
             const double MaxExpense     =   180_000;
             const double ExpSteps       =    10_000;
 
             // Given first year expense, find initial balance.
-            const double FirstYearExp   =   130_000;
             const double MinBalance     = 2_000_000;
             const double MaxBalance     = 5_000_000;
             const double BalanceSteps   =   500_000;
 
+            // Prepare tuples of vary-by inputs
+            List<(double InitBalance, double FirstYearExp)> VaryBy = new();
+            for (double ib = MinBalance; ib <= MaxBalance; ib += BalanceSteps)
+                for (double fye = MinExpense; fye <= MaxExpense; fye += ExpSteps)
+                    VaryBy.Add((ib, fye));
+
             // Base configuration
             var baseConfig = MyBaseConfiguration;
+
+            // Simulate in parallel, collect results
+            var elapsed = Stopwatch.StartNew();
+            var Results = VaryBy
+                .AsParallel()
+                .WithDegreeOfParallelism(Environment.ProcessorCount)
+                .Select(pair => TryOneVariation(baseConfig, pair.InitBalance, pair.FirstYearExp))
+                .OrderBy(x => x.InitBal)
+                .ThenBy(x => x.FYExp)
+                .ToList();
+            elapsed.Stop();
+
+            Console.WriteLine($"{Results.Count} Simulations | Elapsed: {elapsed.Elapsed.TotalMilliseconds:#,0} mSec");
 
             using (var writer = File.CreateText(Path.Combine(MultipleSimulations.ReportsFolder, ReportFileName)))
             {
                 var p = baseConfig.ReadAndValidateRequiredSestion<SimParams>();
                 writer.PrintMarkdownTitle3($"From {p.StartAge} to {p.StartAge + p.NoOfYears} | {p.NoOfYears} years");
 
-                using var w111 = new StringWriter();
-                using var w222 = new StringWriter();
-                using var w333 = new StringWriter();
-
-                var t1 = TrySurvivalMatrix(w111, baseConfig);
-                var t2 = TryDifferentFirstYearExpense(w222, baseConfig);
-                var t3 = TryDifferentInitialBalance(w333, baseConfig);
-
-                await Task.WhenAll(t1, t2, t3);
-
-                writer.Write(w111.ToString());
-                writer.Write(w222.ToString());
-                writer.Write(w333.ToString());
+                PrintSurvivalMatrix(writer, Results);
+                PrintMetricsForSelectInitialBalance(writer, Results, anchorInitialBalance: 3_000_000);
+                PrintMetricsForSelectFirstYearExpense(writer, Results, anchorFirstYearExpense: 130_000);
             }
 
-            return;
-
-            static async Task TrySurvivalMatrix(TextWriter writer, IConfiguration baseConfiguration)
+            // Test one input combination, collect key metrics from the Simulation result
+            static MultiSimResult TryOneVariation(IConfiguration baseConfiguration, double initialBalance, double firstYearExp)
             {
-                await Task.Yield();
+                var overrides = SimInputOverrides
+                    .For<Initial>()
+                        .With(x => x.PreTax.Amount, initialBalance / 2)
+                        .With(x => x.PostTax.Amount, initialBalance / 2)
+                    .For<LivingExpenses>()
+                        .With(x => x.FirstYearAmount, firstYearExp);
 
-                Console.WriteLine($"TrySurvivalMatrix - Begin @ {DateTime.UtcNow:hh:mm:ss}");
+                var simResult = RunSimulation(baseConfiguration, overrides);
 
-                double[] FirstYearExpenses = [120000, 130000, 140000, 150000, 160000];
-                double[] InitBalances = [ 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 ];
-
-                var dtMatrix = new DataTable();
-                dtMatrix.WithColumn<string>("First year Exp");
-                foreach (var ib in InitBalances) dtMatrix.WithColumn<double>($"{ib:C1} M", format: "P0");
-
-                for (double firstYearExp = MinExpense; firstYearExp <= MaxExpense; firstYearExp += ExpSteps)
+                return new MultiSimResult
                 {
+                    InitBal = initialBalance,
+                    FYExp = firstYearExp,
+                    SurvivalRate = simResult.SurvivalRate,
+                    RBal5th = simResult.IterationAtPercentile(0.05).EndingBalanceReal,
+                    RBal10th = simResult.IterationAtPercentile(0.10).EndingBalanceReal,
+                    RBal20th = simResult.IterationAtPercentile(0.20).EndingBalanceReal
+                };
+            }
+
+            static void PrintSurvivalMatrix(TextWriter writer, IList<MultiSimResult> results)
+            {
+                // WHY: Do not trust the ordering guarentee from upstream.
+                // It is a small list, let us sort it here to avoid pinky-promise.
+                results = results.OrderBy(x => x.FYExp).ThenBy(x => x.InitBal).ToList();
+
+                // FYI: .NET preserves the order when using Distinct()
+                // FYI: .NET preserves the order of inner elements when using GroupBy()
+                var colLabels = results.Select(x => x.InitBal).Distinct().ToList();
+                var rowGroups = results
+                    .GroupBy(x => x.FYExp)
+                    .OrderBy(x => x.Key)
+                    .ToList();
+
+                var dtMatrix = new DataTable().WithColumn<string>("First Year Exp");
+                foreach (var colLabel in colLabels) dtMatrix.WithColumn<double>( $"{colLabel/1000000:C1} M", format: "P0");
+
+                foreach(var grp in rowGroups)
+                {
+                    var fyExp = grp.Key;
+                    var sRates = grp.Select(x => x.SurvivalRate);
+
                     var cells = new List<object>();
-                    cells.Add($"{firstYearExp:C0}");
-
-                    for (double initBalance = MinBalance; initBalance <= MaxBalance; initBalance += BalanceSteps)
-                    {
-                        var overrides = SimInputOverrides
-                            .For<Initial>()
-                                .With(x => x.PreTax.Amount, initBalance / 2)
-                                .With(x => x.PostTax.Amount, initBalance / 2)
-                            .For<LivingExpenses>()
-                                .With(x => x.FirstYearAmount, firstYearExp);
-
-                        var survivalRate = RunSimulation(baseConfiguration, overrides).SurvivalRate;
-                        cells.Add(survivalRate);
-                    }
+                    cells.Add($"{fyExp/1000:C0} K");
+                    foreach (var r in sRates) cells.Add(r);
 
                     dtMatrix.Rows.Add(cells.ToArray());
                 }
@@ -88,106 +114,77 @@ namespace UnitTests.WhatIf
                     .PrintMarkdownTable(dtMatrix)
                     .AppendLine();
 
-                Console.WriteLine($"TrySurvivalMatrix - Done @ {DateTime.UtcNow:hh:mm:ss}");
             }
 
-            static async Task TryDifferentFirstYearExpense(TextWriter writer, IConfiguration baseConfiguration)
+            static void PrintMetricsForSelectInitialBalance(TextWriter writer, IList<MultiSimResult> results, double anchorInitialBalance)
             {
-                await Task.Yield();
+                var filteredResults = results
+                    .Where(x => x.InitBal == anchorInitialBalance)
+                    .OrderBy(x => x.FYExp)
+                    .ToList();
 
-                Console.WriteLine($"TryDifferentFirstYearExpense - Begin @ {DateTime.UtcNow:hh:mm:ss}");
-
-                // Prepare output table
                 var dt = new DataTable()
                     .WithColumn<double>("Y0-Expense", format: "C0")
                     .WithColumn<double>("SurvivalRate", format: "P0")
                     .WithColumn<double>("Balance(r) 5th", format: "C0")
                     .WithColumn<double>("Balance(r) 10th", format: "C0")
-                    .WithColumn<double>("Balance(r) 20th", format: "C0")
-                    ;
+                    .WithColumn<double>("Balance(r) 20th", format: "C0");
 
-                // Try different first-year-expense
-                for (double expense = MinExpense; expense <= MaxExpense; expense += ExpSteps)
+                foreach (var r in filteredResults)
                 {
-                    var overrides = SimInputOverrides
-                        .For<Initial>()
-                            .With(x => x.PreTax.Amount, InitBalance / 2)
-                            .With(x => x.PostTax.Amount, InitBalance / 2)
-                        .For<LivingExpenses>()
-                            .With(x => x.FirstYearAmount, expense);
-
-                    var simResult = RunSimulation(baseConfiguration, overrides);
-
                     dt.AppendRow(
                     [
-                        expense,
-                        simResult.SurvivalRate,
-                        simResult.IterationAtPercentile(0.05).EndingBalanceReal,
-                        simResult.IterationAtPercentile(0.10).EndingBalanceReal,
-                        simResult.IterationAtPercentile(0.20).EndingBalanceReal
+                        r.FYExp,
+                        r.SurvivalRate,
+                        r.RBal5th,
+                        r.RBal10th,
+                        r.RBal20th
                     ]);
                 }
 
                 writer
-                    .PrintMarkdownTitle2("Frst year expense vs survival rate")
-                    .PrintMarkdownTitle3($"Initial balance: {InitBalance:C0}")
+                    .PrintMarkdownTitle2("First year expense vs survival rate")
+                    .PrintMarkdownTitle3($"Initial balance: {anchorInitialBalance:C0}")
                     .PrintMarkdownTitle3("Results:")
                     .PrintMarkdownTable(dt)
                     .AppendLine();
-
-                Console.WriteLine($"TryDifferentFirstYearExpense - Done @ {DateTime.UtcNow:hh:mm:ss}");
             }
 
-            static async Task TryDifferentInitialBalance(TextWriter writer, IConfiguration baseConfiguration)
+            static void PrintMetricsForSelectFirstYearExpense(TextWriter writer, IList<MultiSimResult> results, double anchorFirstYearExpense)
             {
-                await Task.Yield();
+                var filteredResults = results
+                    .Where(x => x.FYExp == anchorFirstYearExpense)
+                    .OrderBy(x => x.InitBal)
+                    .ToList();
 
-                Console.WriteLine($"TryDifferentInitialBalance - Begin @ {DateTime.UtcNow:hh:mm:ss}");
-
-                // Prepare output table
                 var dt = new DataTable()
                     .WithColumn<string>("InitialBalance")
                     .WithColumn<double>("SurvivalRate", format: "P0")
                     .WithColumn<double>("Balance(r) 5th", format: "C0")
                     .WithColumn<double>("Balance(r) 10th", format: "C0")
-                    .WithColumn<double>("Balance(r) 20th", format: "C0")
-                    ;
+                    .WithColumn<double>("Balance(r) 20th", format: "C0");
 
-                // Sweep range
-                for (double initialBalance = MinBalance; initialBalance <= MaxBalance; initialBalance += BalanceSteps)
+                foreach (var r in filteredResults)
                 {
-                    var overrides = SimInputOverrides
-                        .For<Initial>()
-                            .With(x => x.PreTax.Amount, initialBalance / 2)
-                            .With(x => x.PostTax.Amount, initialBalance / 2)
-                        .For<LivingExpenses>()
-                            .With(x => x.FirstYearAmount, FirstYearExp);
-
-                    var simResult = MultipleSimulations.RunSimulation(baseConfiguration, overrides);
-
-                    // if (simResult.SurvivalRate < MinSurvivalRate) continue;
-
                     dt.AppendRow(
                     [
-                        $"{initialBalance / 1000000:C1} M",
-                        simResult.SurvivalRate,
-                        simResult.IterationAtPercentile(0.05).EndingBalanceReal,
-                        simResult.IterationAtPercentile(0.10).EndingBalanceReal,
-                        simResult.IterationAtPercentile(0.20).EndingBalanceReal
+                        $"{r.InitBal / 1000000:C1} M",
+                        r.SurvivalRate,
+                        r.RBal5th,
+                        r.RBal10th,
+                        r.RBal20th
                     ]);
                 }
 
                 writer
                     .PrintMarkdownTitle2("Initial Balance vs Survival Rate")
-                    .PrintMarkdownTitle3($"First year expense: {FirstYearExp:C0}")
-
+                    .PrintMarkdownTitle3($"First year expense: {anchorFirstYearExpense:C0}")
                     .PrintMarkdownTitle3("Results")
                     .PrintMarkdownTable(dt)
                     .AppendLine();
-
-                Console.WriteLine($"TryDifferentInitialBalance - Done @ {DateTime.UtcNow:hh:mm:ss}");
             }
-
         }
     }
 }
+
+
