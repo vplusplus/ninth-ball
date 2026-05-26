7ce7ed490e75302ca7450ae5f85d1d226473288c
Venkatesh Ramakrishnan <vplusplus@live.com>
2026-05-25 22:48:27 -0400

    What-if output optmizations

--- src/UnitTests/WhatIf/WhatIfSimulations.cs
+++ src/UnitTests/WhatIf/WhatIfSimulations.cs
@@ -97,18 +97,89 @@ namespace UnitTests.WhatIf
             elapsed.Stop();
             Console.WriteLine($"Completed {whatIfResults.Count:#,0} simulations | Elapsed: {elapsed.Elapsed.TotalSeconds:#,0} sec");
 
+            // Run single simulation for the target values
+            Variant targetInput = new(
+                whatIfOptions.InitialBalance.Target,
+                whatIfOptions.FirstYearExpense.Target,
+                (int)whatIfOptions.StartAge.Target,
+                baseParams.NoOfYears - ((int)whatIfOptions.StartAge.Target - baseParams.StartAge)
+            );
+            SimResult targetResult = RunOneSimulation(baseConfig, targetInput);
+
+
+
+
             // Extract common assumptions.
             // Group the strategies by its text, keep only common items, i.e. group count > 1
             string[] commonAssumptions = strategies
                 .Where(x => !x.StartsWith("Living expenses"))
                 .ToArray();
 
+
+            var target20 = targetResult.IterationAtPercentile(0.20);
+
             var output = new
             {
-                Description = "Represents Monte Carlo what-if simulation results. WhatIfOptions represents range of questions asked. WhatIfResults represents key metrics of each Monte Carlo simulation.",
+                Description = WhatIfDescription,
                 Assumptions = commonAssumptions,
                 WhatIfOptions = whatIfOptions,
-                WhatIfResults = whatIfResults
+                WhatIfResults = whatIfResults,
+
+                Target = new
+                {
+                    targetInput.InitialBalance,
+                    targetInput.FirstYearExpense,
+                    targetInput.StartAge,
+                    targetInput.NoOfYears,
+
+                    NoOfIterations = targetResult.Iterations.Count(),
+                    SurvivalRate = Math.Round(targetResult.SurvivalRate, 2),
+
+                    RealBalance05thPercentile = targetResult.IterationAtPercentile(0.05).EndingBalanceReal.RoundToMultiples(1000.0),
+                    RealBalance10thPercentile = targetResult.IterationAtPercentile(0.10).EndingBalanceReal.RoundToMultiples(1000.0),
+                    RealBalance20thPercentile = targetResult.IterationAtPercentile(0.20).EndingBalanceReal.RoundToMultiples(1000.0),
+                    RealBalance50thPercentile = targetResult.IterationAtPercentile(0.50).EndingBalanceReal.RoundToMultiples(1000.0),
+                    RealBalanceByIteration = targetResult.Iterations.AsEnumerable().Select(x => x.EndingBalanceReal.RoundToMultiples(1000.0)).ToArray(),
+
+                    YearByYear20thPercentile = targetResult.IterationAtPercentile(0.20).ByYear.ToArray().Select(x => new 
+                    { 
+                        x.Year,
+                        x.Age,
+
+                        JanPreTaxBalanceNominal = x.Jan.PreTax.Amount.RoundToMultiples(1000.0),
+                        JanPostTaxBalanceNominal = x.Jan.PostTax.Amount.RoundToMultiples(1000.0),
+
+                        SocialSecurityIncome = x.Incomes.SS,
+                        AnnuityIncome = x.Incomes.Ann,
+
+                        PYTaxesTotal = x.Taxes.Total.RoundToMultiples(100.0),
+                        PYTaxesFederal = x.Taxes.Federal.Tax.RoundToMultiples(100.0),
+                        PYTaxesState = x.Taxes.State.Tax.RoundToMultiples(100.0),
+                        PYTaxPCT = Math.Round(x.Taxes.TaxPCT, 3),
+                        PYTaxPCTFederal = Math.Round(x.Taxes.TaxPCTFed, 3),
+                        PYTaxPCTState = Math.Round(x.Taxes.TaxPCTState, 3),
+
+                        LivingExpense = x.Expenses.LivExp,
+
+                        WithdrawalsPreTax = x.Withdrawals.PreTax.RoundToMultiples(100.0),
+                        WithdrawalsPostTax = x.Withdrawals.PostTax.RoundToMultiples(100.0),
+
+                        DepositsPostTax = x.Deposits.PostTax.RoundToMultiples(100.0),
+
+                        StocksROI = Math.Round(x.ROI.StocksROI, 3),
+                        BondsROI = Math.Round(x.ROI.BondsROI, 3),
+                        InflationRate = Math.Round(x.ROI.InflationRate, 3),
+                        ChangePreTax = x.Change.PreTax.RoundToMultiples(10.0),
+                        ChangePostTax = x.Change.PostTax.RoundToMultiples(10.0),
+
+                        DecPreTaxBalanceNominal = x.Dec.PreTax.Amount.RoundToMultiples(1000.0),
+                        DecPostTaxBalanceNominal = x.Dec.PostTax.Amount.RoundToMultiples(1000.0),
+
+                        DecBalanceReal = x.DecReal.RoundToMultiples(1000.0),
+                        RunningInflationMultiplier = Math.Round(x.InflationIndex.Consumer, 2),
+
+                    }).ToArray(),
+                }
             };
 
             // Serialize and save as json
@@ -125,6 +196,18 @@ namespace UnitTests.WhatIf
         }
 
         static WhatIfResult TryOneVariation(IConfiguration baseConfiguration, Variant varyBy, HashSet<string> strategies)
+        {
+            var simResult = RunOneSimulation(baseConfiguration, varyBy);
+
+            lock (strategies)
+            {
+                foreach (var strategy in simResult.Strategies) strategies.Add(strategy);
+            }
+
+            return new(simResult);
+        }
+
+        static SimResult RunOneSimulation(IConfiguration baseConfiguration, Variant varyBy)
         {
             var overrides = SimInputOverrides
                 .For<SimParams>()
@@ -147,16 +230,29 @@ namespace UnitTests.WhatIf
 
             using (var session = builder.Build())
             {
-                // Run the simulation
                 var simResult = session.Services.GetRequiredService<ISimulation>().Run();
-
-                // Accumulate strategies to a common list.
-                lock(strategies) {
-                    foreach(var strategy in simResult.Strategies) strategies.Add(strategy);
-                }
-
-                return new(simResult);
+                return simResult;
             }
         }
+
+
+        const string WhatIfDescription = @"
+# Monte Carlo financial planning 'what-if' simulation results.
+
+## Report elements:
+* Assumptions represents simulation strategies that are common across all simulations
+* WhatIfOptions represents range of questions asked. 
+* WhatIfResults represents key metrics of each Monte Carlo simulation against the what-if questions.
+* While the WhatIf elements provides the big picture, the Target elements takes a deeper dive into ONE chosen scenario.
+* Target element represents result of single simulation for chosen target scenario
+* Target.RealBalanceByIteration is real ending balances of each iteration, that can explain fat tail and skewness
+* Target.YearByYear20thPercentile explains what happened year by year on the 20th percentile iteration of the chosen target scenario
+
+## Guidence:
+* Consider displaying survival rates are percentage without decimal places. Example: 95%, 98%, etc.
+* When presenting survival matrix, if the survival rate is less than 90%, display blank.
+
+";
+
     }
 }
